a329ee9cf01b8ac5cba144fce9183c465047738a
Rafaela Valerio <rafaela.valerio@sou.ucpel.edu.br>
2021-10-21 15:39:21 -0300

    get, post ok

--- src/routes/carRouter.js
+++ src/routes/carRouter.js
@@ -3,5 +3,6 @@ const createValidation = require('../app/validation/create');
 
 module.exports = (server, routes, prefix = '/') => {
   routes.get('/' , CarController.getAll);
+  routes.post('/', CarController.create)
   server.use(prefix, routes);
 }
